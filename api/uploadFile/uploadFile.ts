a060e1e3e10ba57965da6490db08c095b28b0e7d
Bartosz <bartosz.janiuk@tsh.io>
2023-03-26 13:44:38 +0200

    change path to serialize api

--- api/uploadFile/uploadFile.ts
+++ api/uploadFile/uploadFile.ts
@@ -1,18 +1,17 @@
-import { SerializedBook } from '@/components/UploadFile/UploadFile.types';
-import axios, { AxiosResponse } from 'axios';
+import { SerializedBook } from "@/components/UploadFile/UploadFile.types";
+import axios, { AxiosResponse } from "axios";
 export const uploadFileAction = (
-    fileToUpload: File
-  ): Promise<AxiosResponse<SerializedBook>> => {
-    const formData = new FormData();
-    formData.append("file", fileToUpload);
-    return axios.post(
-      "http://localhost:1337/api/v1/serializer/serialize",
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
-  };
-  
+  fileToUpload: File
+): Promise<AxiosResponse<SerializedBook>> => {
+  const formData = new FormData();
+  formData.append("file", fileToUpload);
+  return axios.post(
+    "https://pdf-serializer.onrender.com/api/v1/serializer/serialize",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+};
